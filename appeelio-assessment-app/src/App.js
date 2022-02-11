148a89daa38e2631eb933645566e701328073ac7
axelmusch <44018738+axelmusch@users.noreply.github.com>
2022-02-11 08:57:53 +0100

    initial commit

---
+++ appeelio-assessment-app/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App text-main font-bold text-3xl">
+      test
+    </div>
+  );
+}
+
+export default App;
---
+++ appeelio-assessment-app/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      "main": "#0d1117",
+      "accent": "#161b22",
+    }
+  },
+  plugins: [],
+}
